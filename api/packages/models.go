025681df41c976826553c1de0c90507d5d333500
Lucas Bravi <lbravi@mobilecomputing.com.ar>
2023-02-03 17:50:59 -0300

    Se soluciona bug al mostrar envases mostraba todo 0 el id

--- api/packages/models.go
+++ api/packages/models.go
@@ -3,7 +3,7 @@ package packages
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Package struct {
-	ID       primitive.ObjectID `bson:"id,omitempty" json:"id,omitempty"`
+	ID       primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
 	Metric   string             `bson:"metric" json:"metric" validate:"required"`
 	Quantity float32            `bson:"quantity" json:"quantity" validate:"required"`
 }
